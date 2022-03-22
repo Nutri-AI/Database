c88977a928ab12698eaaca2f1ccd36e288a14237
Jiyoon <kjiy8n@gmail.com>
2022-03-22 13:30:40 +0900

    apply .gitignore

---
+++ aws_def_values.py
@@ -0,0 +1,15 @@
+
+## access dict
+aws_access = {
+    'region_ap': 'ap-northeast-2', # Seuol
+    'aws_access_key_id': '', # AWS AccessKeyId 입력
+    'aws_secret_access_key': '' # AWS SecretKey 입력
+}
+
+
+# DynamoDB
+table_nutriai = # table name
+
+
+# S3
+bucket_nutriai = # bucket name
